634ca5481e3f14b1762bc2a9a841d83a32f26439
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2024-10-10 12:45:15 -0700

    feat: simple implementation of a bionic reader

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -2,10 +2,33 @@
  * Implement your converter function here.
  */
 const diyOnicConverter = (textContentContainerSelector) => {
-  const container = document.querySelector(textContentContainerSelector);
-  console.log("Performing bionic reading conversion on:", container);
+	const container = document.querySelector(textContentContainerSelector);
+	if (!container) {
+		console.error(
+			"Invalid selector. Container with the following selector is not found:",
+			textContentContainerSelector
+		);
+		return;
+	}
+	console.log("Performing bionic reading conversion on:", container);
 
-  /* Etc. etc. etc. your code etc. */
+	const paragraphs = container.querySelectorAll("p");
+
+	paragraphs.forEach((paragraph) => {
+		const words = paragraph.textContent.split(" ");
+		const bionicWords = words.map((word) => {
+			if (word.length > 1) {
+				const highlightLength = Math.ceil(word.length / 2);
+				return `<strong>${word.slice(0, highlightLength)}</strong>${word.slice(
+					highlightLength
+				)}`;
+			}
+			// Keep single character as is, highlight is not needed
+			return word;
+		});
+
+		paragraph.innerHTML = bionicWords.join(" ");
+	});
 };
 
 // Allow global access so that this can be executed from the console.
